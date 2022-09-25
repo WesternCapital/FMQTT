2101fb5974a81d24250a891a883f8a9f6556aeb7
fischgeek <fischgeek@gmail.com>
2022-09-25 12:27:01 -0500

    added example

---
+++ Example.fs
@@ -0,0 +1,13 @@
+﻿namespace FMQTT
+
+open FMQTT.FMQTT
+
+module Example = 
+
+    // Connect to local mosquitto broker
+    let ConnectToLocal () : MyMQTT = 
+        MyMQTT.New
+        |> MyMQTT.SetUrl "localhost" 1883
+        |> MyMQTT.SetCredentials "mosquitoo" ""
+        |> MyMQTT.Connect
+
