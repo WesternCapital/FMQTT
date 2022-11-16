7b41573826544980ecc738ce0dd914af1ac0874f
PaulWCRI <pauls@wcrimail.com>
2022-11-16 12:57:45 -0600

    quick

--- FMQTT.fs
+++ FMQTT.fs
@@ -2,6 +2,8 @@
 
 open MQTTnet.Client
 open MQTTnet
+//open Protocol
+open MQTTnet.Protocol
 open System.Threading
 open System.Threading.Tasks
 open MQTTnet
@@ -10,6 +12,31 @@ open MQTTnet.Packets
 open System.Collections.Generic
 
 module FMQTT = 
+    type ClientModel<'a> = 
+        {
+            NoLocal           : bool
+            RetainAsPublished : bool
+            Topic: string
+            OnChangeWeak: string -> unit
+            OnChangeStrong: 'a -> unit
+            SendOnSubcribe: MqttRetainHandling
+        }
+        static member Create topic =
+            {
+                NoLocal = false
+                RetainAsPublished = false
+                OnChangeStrong = ignore
+                OnChangeWeak = ignore
+                SendOnSubcribe = MqttRetainHandling.DoNotSendOnSubscribe
+                Topic = topic
+            }
+    type ClientBuilder =
+        static member NoLocal            (b: ClientModel<_>) = { b with NoLocal            = true }
+        static member SendOnSubcribe            (b: ClientModel<_>) = { b with SendOnSubcribe            = MqttRetainHandling.SendAtSubscribeIfNewSubscriptionOnly }
+        static member RetainAsPublished  (b: ClientModel<_>) = { b with RetainAsPublished  = true }
+        static member Topic x (b: ClientModel<_>) = { b with Topic = x }
+        static member OnChange (x: 'a -> unit) (b: ClientModel<'a>) = { b with OnChangeStrong = x }
+
     let tee (x: 'obj -> unit) (y: 'obj) : 'obj =
         x y
         y
@@ -17,6 +44,7 @@ module FMQTT =
     let (|--) a b = a |> tee b
 
     type MQTTMessageEventArgs = MqttApplicationMessageReceivedEventArgs
+
     type MyMQTT =
         {
             BrokerName: string
@@ -48,18 +76,45 @@ module FMQTT =
         static member SetCredentials (user: string) (pass: string) (mq: MyMQTT) = {mq with OptionsBuilder = mq.OptionsBuilder.WithCredentials(user, pass)}
         static member UseTLS (mq: MyMQTT) = {mq with OptionsBuilder = mq.OptionsBuilder.WithTls()}
         static member Connect (mq: MyMQTT) = 
-            printf $"Connecting to broker: {mq.BrokerName}..."
+            //printf $"Connecting to broker: {mq.BrokerName}..."
             mq.Client.ConnectAsync(mq.OptionsBuilder.Build(), CancellationToken.None).Wait()
             //printfn "Connected!"
             mq
+        
+        member this.SubscribeToTopicWithModel (b: ClientModel<_>) = 
+            let sub =
+                this.Factory.CreateSubscribeOptionsBuilder() 
+                |> fun x -> 
+                    MqttTopicFilterBuilder()
+                    |> fun x -> x.WithRetainHandling b.SendOnSubcribe
+                    |> fun x -> x.WithRetainAsPublished b.RetainAsPublished
+                    |> fun x -> x.WithTopic b.Topic
+                    |> fun x -> x.WithNoLocal b.NoLocal
+                    |> x.WithTopicFilter
+                    |> fun x -> x.Build()
+            this.Client.SubscribeAsync(sub, CancellationToken.None).Wait()
+            this.Client.add_ApplicationMessageReceivedAsync(fun x -> 
+                let y = x.ApplicationMessage.ConvertPayloadToString()
+                b.OnChangeWeak y
+                Task.CompletedTask
+            )
         member this.SubscribeToTopic (topic: string) (fn: MqttApplicationMessageReceivedEventArgs -> unit) = 
             let sub = this.Factory.CreateSubscribeOptionsBuilder() |> fun x -> x.WithTopicFilter(fun f -> f.WithTopic(topic) |> ignore).Build()
             //this.Client.SubscribeAsync((new MqttTopicFilterBuilder()).WithTopic(topic).Build()).Wait()
             //this.Client.SubscribeAsync(sub, CancellationToken.None).Wait()
             this.EventHandlers.Add(topic, fn)
             this.Client.SubscribeAsync(topic).Wait()
+
+        //member this.SubscribeToTopic (topic: string) (fn: string -> unit) = 
+        //    let sub = this.Factory.CreateSubscribeOptionsBuilder() |> fun x -> x.WithTopicFilter(fun f -> f.WithTopic(topic) |> ignore).Build()
+        //    //this.Client.SubscribeAsync((new MqttTopicFilterBuilder()).WithTopic(topic).Build()).Wait()
+        //    //this.Client.SubscribeAsync(sub, CancellationToken.None).Wait()
+        //    this.EventHandlers.Add(topic, fn)
+        //    this.Client.SubscribeAsync(topic).Wait()
+
         member this.SubscribeToTopicBasic (topic: string) (fn: string -> obj) = this.Client.SubscribeAsync(topic).Wait()
+
         member this.PublishMessage (topic: string) (data: string) = 
-            let amb = (new MqttApplicationMessageBuilder()).WithTopic(topic).WithPayload(data).Build()
+            let amb = (new MqttApplicationMessageBuilder()).WithRetainFlag().WithTopic(topic).WithPayload(data).Build()
             //printfn "Sending message to mqtt..."
             this.Client.PublishAsync(amb, CancellationToken.None) |> ignore
