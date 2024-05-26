050ab2c91edbae8e43a5b90f6588beb9babc4785
Paul Staszko <paulstaszko@gmail.com>
2024-05-26 01:02:00 -0500

    ok

--- FMQTT/Example.fs
+++ FMQTT/Example.fs
@@ -8,5 +8,15 @@ module Example =
     let ConnectToLocal () : MqttConnection =
         MqttConnection.New
         |> MqttConnection.SetUrl "localhost" 1883
-        |> MqttConnection.SetCredentials "mosquitoo" ""
+        |> MqttConnection.SetCredentials "mosquitoo" "" //is this a typo "mosquitoo"? Does it matter?
         |> MqttConnection.Connect
+
+[<AutoOpen>]
+module Settings =
+    let private mqtt = lazy MqttConnection.ConnectToEnvironmentMQTT()
+    let MakeToggler = FMQTT.Togglers.MakeTogglerBase mqtt.Value
+
+    let CreateRetainedBool (onChange: _ -> unit) defaultValue topic =
+        //mqtt.Value.EnsureConnected()
+        let v = mqtt.Value
+        MQTTObservable.CreateRetainedBool v onChange defaultValue topic
